cb470822770b790f9c22724426fe24d5412807c8
comefly-in <1252253192@qq.com>
2023-10-25 22:43:02 +0800

    最小操作次数

---
+++ 算法/最小操作次数.js
@@ -0,0 +1,16 @@
+var minMoves2 = function(nums) {
+  let sum = 0
+  let ans = 0
+  nums.sort((a,b) => a - b)
+  
+  let mid = nums[Math.floor(nums.length / 2)]
+  for(let i = 0 ; i < nums.length ; i ++){
+      ans += Math.abs(mid - nums[i])
+  }
+  
+  return ans
+};
+
+
+let nums = [1,0,0,8,6]
+console.log(minMoves2(nums));
