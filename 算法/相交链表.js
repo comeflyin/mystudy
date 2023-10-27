8a192872d08d835bcb7de1cfde817d9a6e3820da
comefly-in <1252253192@qq.com>
2023-10-27 20:45:34 +0800

    '相交链表-双指针算法'

--- 算法/相交链表.js
+++ 算法/相交链表.js
@@ -14,3 +14,26 @@ var getIntersectionNode = function(headA, headB) {
   return null
 };
 
+// 双指针
+var getIntersectionNode = function(headA, headB) {
+    let pa = headA
+    let pb = headB
+    if(pa == null || pb == null){
+        return null
+    }
+    while(pa != pb){
+        
+        
+        if(pa == null){
+            pa = headB
+        }else(
+            pa = pa.next
+        )
+        if(pb == null){
+            pb = headA
+        }else{
+            pb = pb.next
+        }
+    }
+    return pa
+};
