511c222a66299beead5ac9dc37a5e276b26b7d3b
comefly-in <1252253192@qq.com>
2023-10-30 19:06:17 +0800

    '学习'

--- 算法/实验.js
+++ 算法/实验.js
@@ -1,9 +1,3 @@
-console.log(Foo.prototype.constructor === Foo) // ?
-console.log(f1.__proto__ === Foo.prototype) // ?
 
-// 难度中
-console.log(Object.prototype.__proto__ === null) // ?
-console.log(Function.prototype.__proto__ === Object.prototype) // ?
-
-// 难度较大
-console.log(Function.prototype === Object.__proto__) // ?
+let list = new ListNode(1);
+let lis
