760956e40486190bd71856b594f929a3677cb63d
comefly-in <1252253192@qq.com>
2023-10-31 17:34:12 +0800

    '最长连续序列'

---
+++ 算法/最长连续序列.js
@@ -0,0 +1,14 @@
+var longestConsecutive = function(nums) {
+  let map = new Map()
+  let max = 0
+   nums.sort((a, b) => a - b);
+  for(let i = 0;i < nums.length; i ++){
+      if(map.has(nums[i] - 1)){
+         map.set(nums[i],map.get(nums[i] - 1) + 1)   
+      }else{
+          map.set(nums[i],1)
+      }
+  max = Math.max(max, map.get(nums[i]));
+  }
+  return max
+};
