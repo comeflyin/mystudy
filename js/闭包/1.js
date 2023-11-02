632e8c219d8fb0441d48f223ffa8686001c008ea
comefly-in <1252253192@qq.com>
2023-11-02 23:33:44 +0800

    '学习记录'

--- js/闭包/1.js
+++ js/闭包/1.js
@@ -1,32 +1,30 @@
-// var arr = []
-// var c = []
-// for(var i = 0; i < 10; i++) {
-//     arr[i] = function() {
-//         var b = i
-//         var a = function(){
-//             console.log(b)
-//         }
-//        return a
-//     }
-//     c[i] = arr[i]()
-// }
-
-// //
-
-// for(let j = 0; j < 10; j++) {
-//      c[j]()
-// }
-
 var arr = []
 
 for(var i = 0; i < 10; i++) {
-    (function(i){
-      arr[i] = function() {
-        console.log(i)
+  function a(i){
+    arr[i] = function() {
+      console.log(i)
     }
-    })(i)
+  }
+  a(i)
 }
+
+//
 
 for(let j = 0; j < 10; j++) {
      arr[j]()
- }
+}
+
+// var arr = []
+
+// for(var i = 0; i < 10; i++) {
+//     (function(i){
+//       arr[i] = function() {
+//         console.log(i)
+//     }
+//     })(i)
+// }
+
+// for(let j = 0; j < 10; j++) {
+//      arr[j]()
+//  }
