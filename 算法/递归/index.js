5bb8121a6298ca2e1f5d4b27feedb3c2664a695a
comefly-in <1252253192@qq.com>
2023-11-03 20:54:18 +0800

    '学习递归'

--- 算法/递归/index.js
+++ 算法/递归/index.js
@@ -12,18 +12,24 @@
 
 // console.log(fb(10));
 
-var arr = [1,[2,[3,[4,[5,[6,[7,[8,[9,[10]]]]]]]]]]
-function flatten(n){
-  var res = []
-  for(var i = 0; i < n.length; i++){
-    if(Array.isArray(n[i])){
-      res = res.concat(flatten(n[i]))
-    }else{
-      res.push(n[i])
-    }
-  }
-  return res
-}
+// var arr = [1,[2,[3,[4,[5,[6,[7,[8,[9,[10]]]]]]]]]]
+// function flatten(n){
+//   var res = []
+//   for(var i = 0; i < n.length; i++){
+//     if(Array.isArray(n[i])){
+//       res = res.concat(flatten(n[i]))
+//     }else{
+//       res.push(n[i])
+//     }
+//   }
+//   return res
+// }
 
-arrSum(10)
-console.log(arr);
+// arrSum(10)
+// console.log(arr);
+
+function flatten(arr){
+  return arr.reduce((pre,cur)=>{
+    return pre.concat(Array.isArray(cur) ? flatten(cur) : cur)
+  },[])
+}
