e697f3bbe18a8cfa6e36864e8accc56c130716f9
comefly-in <1252253192@qq.com>
2023-11-03 16:11:07 +0800

    '3的幂'

--- 算法/递归/index.js
+++ 算法/递归/index.js
@@ -5,9 +5,25 @@
 
 // console.log(mul(5));
 
-function fb(n){
-  if(n === 1 || n === 2) return 1
-  return fb(n-1) + fb(n-2)
+// function fb(n){
+//   if(n === 1 || n === 2) return 1
+//   return fb(n-1) + fb(n-2)
+// }
+
+// console.log(fb(10));
+
+var arr = [1,[2,[3,[4,[5,[6,[7,[8,[9,[10]]]]]]]]]]
+function flatten(n){
+  var res = []
+  for(var i = 0; i < n.length; i++){
+    if(Array.isArray(n[i])){
+      res = res.concat(flatten(n[i]))
+    }else{
+      res.push(n[i])
+    }
+  }
+  return res
 }
 
-console.log(fb(10));
+arrSum(10)
+console.log(arr);
