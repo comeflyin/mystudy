871f3c4582748248b3caa112e4a07ecc7aa119c0
comefly-in <1252253192@qq.com>
2023-11-05 19:51:39 +0800

    '盛水最多的容器'

---
+++ 算法/盛水最多的容器.js
@@ -0,0 +1,14 @@
+var maxArea = function(height) {
+  let l = 0
+  let r = height.length - 1
+  let res = 0
+  while(l < r){
+      res = Math.max(res,Math.min(height[l],height[r]) * (r - l))
+      if(height[l] < height[r]){
+          l ++
+      }else{
+          r --
+      }
+  }
+  return res
+};
