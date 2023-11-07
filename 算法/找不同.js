631c4817f9a9d0a48d04021c07d8cd8989f7ea99
comefly-in <1252253192@qq.com>
2023-11-07 23:21:36 +0800

    '找不同'

---
+++ 算法/找不同.js
@@ -0,0 +1,21 @@
+var findTheDifference = function(s, t) {
+  let map = new Map()
+  for(let i = 0;i < s.length;i ++){
+      if(map.has(s[i])){
+          map.set(s[i],map.get(s[i]) + 1)
+      }else{
+          map.set(s[i],1)
+      }
+  }
+
+  for(let i = 0;i < t.length;i ++){
+      if(map.has(t[i])){
+          if(map.get(t[i]) === 0) return t[i]
+          map.set(t[i],map.get(t[i]) - 1)
+      }else{
+          return t[i]
+      }
+  }
+};
+
+
