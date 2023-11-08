79dd00d891700fe317504530488d44acfc804966
comefly-in <1252253192@qq.com>
2023-11-08 19:06:43 +0800

    '字符串中的第一个唯一字符'

--- 算法/字符串中的第一个唯一字符.js
+++ 算法/字符串中的第一个唯一字符.js
@@ -12,3 +12,21 @@ var firstUniqChar = function(s) {
   if(ans[0] !== undefined) return ans[0]
   return -1
 };
+
+
+
+var firstUniqChar = function(s) {
+  let map = new Map()
+  let ans = []
+  for(let i = 0;i < s.length;i ++){
+      if(map.has(s[i])){
+          map.set(s[i],map.get(s[i]) + 1)
+      }else{
+          map.set(s[i],1)
+      }
+  }
+  for(let i = 0;i < s.length;i ++){
+      if(map.get(s[i]) === 1) return i
+  }
+  return -1
+};
