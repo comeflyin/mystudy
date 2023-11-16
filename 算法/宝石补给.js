1e69fefbe16c8c6e4facd2451dfdfeaad86b2e0c
comefly-in <1252253192@qq.com>
2023-11-16 23:30:37 +0800

    '宝石补给'

---
+++ 算法/宝石补给.js
@@ -0,0 +1,12 @@
+var giveGem = function(gem, operations) {
+  for (const operation of operations) {
+      const x = operation[0];
+      const y = operation[1];
+      const number = Math.floor(gem[x] / 2);
+      gem[x] -= number;
+      gem[y] += number;
+  }
+  const mn = Math.min(...gem);
+  const mx = Math.max(...gem);
+  return mx - mn;
+};
