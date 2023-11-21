974050b4eca1f4b8be736a1f72c41d903084f098
comefly-in <1252253192@qq.com>
2023-11-21 20:44:24 +0800

    '使用最小花费爬楼梯'

---
+++ js/类型转换/1.js
@@ -0,0 +1,19 @@
+// number string  boolean  undefined  null  object ES6(bigInt symbol)  
+
+// console.log(Boolean()); // 默认false
+// console.log(Boolean(1)); // true
+// console.log(Boolean(undefined)); // false
+// console.log(Boolean(null)); // false
+// console.log(Boolean('123')); // true
+// console.log(Boolean('')); // false
+// console.log(Boolean(' ')); // true
+// console.log(Boolean({})); // true
+// console.log(Boolean([])); // true
+
+// console.log(Number()); // 0
+// console.log(Number('123')); // 123
+// console.log(Number('hello')); // NaN
+// console.log(Number(undefined)); // NaN
+// console.log(Number(null)); // 0
+// console.log(Number(true)); // 1
+
