acbcf8c41c1dc773c6c05fccceeea46e62fde175
comefly-in <1252253192@qq.com>
2023-11-29 16:15:04 +0800

    'vue指令on和bind'

--- 算法/实验.js
+++ 算法/实验.js
@@ -1,42 +1,8 @@
-var fourSum = function(nums, target) {
-  const len = nums.length
-  let ans = []
-  if(len < 4){
-      return ans
-  }
-  nums.sort((x, y) => x - y)
-  for(let i = 0; i < len - 3; i++){
-      if(i > 0 && nums[i] === nums[i - 1]){
-          continue
-      }
-      for(let j = i + 1; j < len - 2; j++){
-          if(j > i + 1 && nums[j] === nums[j - 1]){
-              continue
-          }
-          let left = j + 1, right = len - 1   
-          while(left < right){
-              let sum = nums[i] + nums[j] + nums[left] + nums[right]
-              if(sum === target){
-                  ans.push([nums[i], nums[j], nums[left], nums[right]])
-                  while(left < right && nums[left] === nums[left + 1]){
-                      left++
-                  }
-                  left++
-                  while(left < right && nums[right - 1] === nums[right]){
-                      right--
-                  }
-                  right--
-              } else if(sum < target){
-                  left++
-              } else{
-                  right--
-              }
-          }
-      }
-  }
-  return ans
-};
+var decodeString = function (s) {
+    const [self,times,str]  = s.match(/(\d*)\[([^\[\]]*)\]/)||[]
+    console.log(self,times,str);
+   //return self?decodeString(s.replace(self,Array(times*1).fill(str).join(''))):s
+}
 
-nums = [1,0,-1,0,-2,2], target = 0
-
-console.log(fourSum(nums, target));
+let s = "3[a]2[bc]"
+decodeString(s)
