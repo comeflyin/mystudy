e177b9ce6d0222480a26e559e83edec0fab670ce
comefly-in <1252253192@qq.com>
2023-12-02 21:58:56 +0800

    '平方数之和'

---
+++ 算法/平方数之和.js
@@ -0,0 +1,15 @@
+var judgeSquareSum = function(c) {
+  let left = 0;
+   let right = Math.floor(Math.sqrt(c));
+   while (left <= right) {
+       const sum = left * left + right * right;
+       if (sum === c) {
+           return true;
+       } else if (sum > c) {
+           right--;
+       } else {
+           left++;
+       }
+   }
+   return false;
+};
