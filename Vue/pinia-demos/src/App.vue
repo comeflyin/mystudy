52f6899fcfb817e8114a8b681f940333d341d075
comeflyin <1252253192@qq.com>
2023-12-22 21:35:35 +0800

    pinia

---
+++ Vue/pinia-demos/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+// 以组件为开发单元
+import {ref} from 'vue'
+// const todos = ref([
+//   {text:'haha'}
+// ])
+
+// todos 来到 vuex mapState Pinia
+import { useTodoStore } from './store/todo.js'
+import TodoList from './components/TodoList.vue'
+
+
+
+const { todos,addTodo } = useTodoStore() 
+// console.log(todoStore.todos);
+
+</script>
+
+<template>
+  <div>
+    <TodoList 
+    :todos="todos" 
+    :addTodo="addTodo"
+    />
+    <!-- <Item/>
+    <Bang :title="作者榜" :item="authors"/> -->
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ Vue/pinia-demos/src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos">
+        {{ todo.txt }}
+      </li>
+      <button type="button" @click="addTodo">添加</button>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+
+const props =  defineProps({
+  todos:{
+    type:Array
+  },
+  addTodo:{
+    type:Function
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
