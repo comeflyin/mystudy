723a72a0d3d48a78f9755f613de34ed38578f8d5
comeflyin <1252253192@qq.com>
2024-01-02 23:09:57 +0800

    client

---
+++ Vue/node-book/client/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'amfe-flexible' // 自动适配移动端
+import './assets/style/reset.css'
+import { Button } from 'vant';
+// 2. 引入组件样式
+import 'vant/lib/index.css';
+import router from './router'
+import { Form, Field, CellGroup } from 'vant';
+
+const app = createApp(App)
+
+app
+  .use(Form)
+  .use(Field)
+  .use(CellGroup)
+  .use(Button)
+  .use(router)
+  .mount('#app')
